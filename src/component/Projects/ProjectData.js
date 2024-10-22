1699ecde8fbfd374b62b61a54e5bb4a9c3a5c263
kar-thik-m <sparrowkarthik007@gmail.com>
2024-10-22 07:30:50 +0530

    add some

--- src/component/Projects/ProjectData.js
+++ src/component/Projects/ProjectData.js
@@ -2,20 +2,11 @@ import pin from "../../assets/pin.png"
 import makeup from "../../assets/makeup.png";
 import gym from "../../assets/gym.png";
 import ec from "../../assets/ec.png"
+import chatapp from "../../assets/chatapp.png"
 export const ProjectData = () => {
     return [
         {
             id: 1,
-            Projectname: "Your-Shop",
-            use: "This project mini e-commerce ,build by MERN stack",
-            tools: "React.js,Node.js,MongoDb",
-            url: "https://e-commerce-frontend-dbs3.onrender.com/",
-            code1: "https://github.com/Kar-thik-m/e-commerce/tree/main/frontend",
-            code2: "https://github.com/Kar-thik-m/e-commerce/tree/main/backend",
-            image: ec,
-        },
-        {
-            id: 2,
             Projectname: "Pinterest",
             use: "This project allows users to create an account, upload images, download images from others, comment on images, and leave reviews. Users can also follow other accounts.",
             tools: "MongoDB, Express.js, React.js, Node.js",
@@ -23,6 +14,26 @@ export const ProjectData = () => {
             code1: "https://github.com/Kar-thik-m/Printerest/tree/main/backend",
             code2: "https://github.com/Kar-thik-m/Printerest/tree/main/frontend",
             image: pin,
+        },
+        {
+            id: 2,
+            Projectname: "ChatApp",
+            use: "Users can send and receive messages instantly in real-time, enhancing communication efficiency.",
+            tools: "React.js,Socket.IO ,express.js,mongodb database",
+            url: "https://playful-creponne-6f7123.netlify.app/login",
+            code1: "https://github.com/Kar-thik-m/chat-frontend",
+            code2: "https://github.com/Kar-thik-m/chat-backend",
+            image: chatapp,
+        },
+        {
+            id: 3,
+            Projectname: "Your-Shop",
+            use: "This project mini e-commerce ,build by MERN stack",
+            tools: "React.js,Node.js,MongoDb",
+            url: "https://e-commerce-frontend-dbs3.onrender.com/",
+            code1: "https://github.com/Kar-thik-m/e-commerce/tree/main/frontend",
+            code2: "https://github.com/Kar-thik-m/e-commerce/tree/main/backend",
+            image: ec,
         },
         {
             id: 3,
@@ -33,16 +44,10 @@ export const ProjectData = () => {
             code1: "https://github.com/Kar-thik-m/make-up",
             image: makeup,
         },
+        
 
 
-        {
-            id: 4,
-            Projectname: "Gym",
-            use: "This project utilizes gym traing details and is built using the react.js",
-            tools: "React.js",
-            url: "https://stellular-liger-81bee6.netlify.app/",
-            code1: "https://github.com/Kar-thik-m/sample",
-            image: gym,
-        }
+
+
     ];
 };
